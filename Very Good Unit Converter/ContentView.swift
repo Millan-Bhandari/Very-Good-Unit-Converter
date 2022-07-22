a1334d4b07e24f55a153a6c52fbbd439d9210780
Millan Bhandari <millan.ib@icloud.com>
2022-07-22 14:12:39 -0500

    Initial Commit

---
+++ Very Good Unit Converter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Very Good Unit Converter
+//
+//  Created by Millan Bhandari on 7/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Very Good Unit Converter/Very_Good_Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Very_Good_Unit_ConverterApp.swift
+//  Very Good Unit Converter
+//
+//  Created by Millan Bhandari on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Very_Good_Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
